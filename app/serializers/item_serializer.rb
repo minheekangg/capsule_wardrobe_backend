c5f52de63f1c146b0af2e6edc3c56cb6bc3dbdb3
minheekangg <minheekangg@gmail.com>
2019-01-16 11:40:12 -0500

    expose item updated_at

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,5 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :image, :times_worn, :category_id
+  attributes :id, :name, :image, :times_worn, :category_id, :updated_at
   belongs_to :user
   has_many :ootds
 end
